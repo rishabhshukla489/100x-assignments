5d22d7f19321ae965a24061c13ec746dec2636c2
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-18 14:11:08 +0530

    init commit

--- week-1/offline-class-1/level-1/01-String.js
+++ week-1/offline-class-1/level-1/01-String.js
@@ -59,7 +59,7 @@ function trimString(str) {
   console.log("Original String:", str);
   console.log("After trim:", str.trim());
 }
-trimString(" Hello World ");
+trimString("                Hello World ");
 
 // toUpperCase
 function toUpper(str) {
