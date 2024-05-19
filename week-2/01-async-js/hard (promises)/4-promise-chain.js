e0110aa90734ae8b327d0eb40b79027cf7c2e43f
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-20 00:01:28 +0530

    complete week2 promis

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -27,7 +27,7 @@ function calculateTime(t1, t2, t3) {
 const start=Date.now()
 return wait1(t1).then(()=>wait2(t2)).then(()=>wait3(t3)).then(()=>{
     const end=Date.now();
-    return end-start;
+    return end-start
 })
 }
 
