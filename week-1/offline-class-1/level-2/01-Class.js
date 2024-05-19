7185dd15c1f7fddf33f9168eb7d21b2f044d71e6
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-19 18:53:58 +0530

    counter.js

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -4,9 +4,15 @@ class Animal {
     this.name = name
     this.legCount = legCount
   }
+  static my(){
+    console.log("fuck u")
+  }
   describe() {
     return `${this.name} has ${this.legCount} legs`
   }
 }
 
 
+let d= new Animal("r",4)
+console.log(d.describe())
+Animal.my()
