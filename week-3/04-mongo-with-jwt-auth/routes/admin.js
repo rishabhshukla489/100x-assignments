c69bef71f998a7d2f1f5c6fcdd0a1bf385ec8d86
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-29 18:02:06 +0530

    testing

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,27 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../../03-mongo/db");
 const router = Router();
+const jwt=require("jsonwebtoken");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username= req.body.username;
+    const password= req.body.password;
+    await Admin.create({
+        username: username,
+        password: password
+    })
+        res.json({
+            msg: 'Admin created succesffulyy'
+        })
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username= req.body.username;
+    const password= req.body.password;
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
